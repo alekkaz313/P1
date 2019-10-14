5033ce3bfe46d6d86f8a6aa4757bb9fbbf60a179
alekkaz313 <aleolazaqwsx@gmail.com>
2019-10-14 09:44:28 +0200

    Zmiana w projekcie 2

--- src/P1/Main.java
+++ src/P1/Main.java
@@ -2,6 +2,6 @@ package P1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Projekt nr 1 po zmianie");
+        System.out.println("Projekt nr 1 po zmianie w sklonowanym projekcie nr 2");
     }
 }
